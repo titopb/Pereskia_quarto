3eeee89a4ac02af9a7beacc6d5bb3ab7974f16a1
Milton Lima Neto <titopb@gmail.com>
2023-05-01 15:04:52 -0300

    different folders inclued here() for relative path figures in figs

--- script/Box_plot_tukey.R
+++ script/Box_plot_tukey.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(psych)
 library(multcompView)
+library(here)
 
 # treating data
 
-dados <- read.csv("data.csv", stringsAsFactors = T)
+dados <- read.csv(here("data", "data.csv"))
 dados$treatment <- as.factor(dados$treatment)
 dados <- janitor::clean_names(dados)
 
@@ -15,20 +16,12 @@ glimpse(dados)
 variable <- dados$water_potential
 
 
-#descriptive statistics by group
+# descriptive statistics by group
+# from package (psych)
 
 describeBy(variable, group = dados$treatment)
 
-# need to change the parameter caption for each one
-#table_fm <- dados %>% 
- # group_by(treatment) %>% 
-  #summarise(Mean = mean(fm),
-   #         SD = sd(fm),
-    #        median = median(fm))
-
-#table_fm
-
-#ANOVA 
+# ANOVA 
 anova <- aov(variable ~treatment, dados)
 summary(anova)
 
@@ -50,10 +43,13 @@ ggplot(dados, aes(treatment, variable)) +
   geom_errorbar(stat = 'boxplot', width = 0.2) +
   geom_boxplot(aes(fill = treatment), show.legend = F) +
   xlab("") +
-  ylab(expression(Fresh~matter~g.plant^-1))+   # fix legend
+  #ylab(expression(Fresh~matter~g.plant^-1))+  # fix legend
+  ylab(expression(Psi[w]~(MPa))) 
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   scale_fill_brewer(palette = "Set1") +
-  ylim(0,1)                                                       # Fix scale 
+  ylim(0,-1)                                                       # Fix scale 
 
-ggsave("Fig3.tiff", width = 4, height = 3, dpi = 300)
+ggsave(here("figs","water_potential.tiff"), width = 4, height = 3, dpi = 300)
+
+       
