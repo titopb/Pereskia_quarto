ef5e52b9548bb8e484455df6df91c1a3d015fcec
Milton Lima Neto <titopb@gmail.com>
2023-06-22 11:26:51 -0300

    Figuras editadas para melhor visualização em .R ignore arquivos do affinity design update M&M

--- script/statistics_function.R
+++ script/statistics_function.R
@@ -79,8 +79,177 @@ for (measurement in measurements) {
   print(tukey_letters)
 }
 
-# Plot boxplots 
+## Plot boxplots ##
 
+# FvFM
+ggplot(dados, aes(treatment, fvfm)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("FvFm")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,1)      
+
+ggsave(here("figs","fvfm.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+
+# Y(II)
+ggplot(dados, aes(treatment, y_ii)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("Y(II)")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,0.8) 
+
+
+ggsave(here("figs","yii.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# NPQ
+
+ggplot(dados, aes(treatment, npq)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("NPQ")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,2) 
+
+
+ggsave(here("figs","npq.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# 1-qP
+
+ggplot(dados, aes(treatment, x1_q_p)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("1-qP")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,1.5) 
+
+
+ggsave(here("figs","1qP.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# Y(NO)
+
+ggplot(dados, aes(treatment, y_no)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("Y(NO)")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,1.5) 
+
+
+ggsave(here("figs","yno.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# YNPQ
+
+ggplot(dados, aes(treatment, y_npq)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab("Y(NPQ)")+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,1.5) 
+
+
+ggsave(here("figs","ynpq.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# Pn
+
+ggplot(dados, aes(treatment, pn)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab(expression(paste("P"[N], " (", mu, "mol m"^-2, " s"^-1, ")")))+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,10) 
+
+
+ggsave(here("figs","pn.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# gs 
+
+ggplot(dados, aes(treatment, gs)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab(expression(paste("g"[s], " (", "mol m"^-2, " s"^-1, ")")))+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,100) 
+
+
+ggsave(here("figs","gs.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# E
+
+ggplot(dados, aes(treatment, e)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab(expression(paste("E", " (mmol m"^-2, " s"^-1, ")")))+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,3) 
+
+
+ggsave(here("figs","E.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# Ci
+
+ggplot(dados, aes(treatment, ci)) +
+  geom_errorbar(stat = 'boxplot', width = 0.2) +
+  geom_boxplot(aes(fill = treatment), show.legend = F) +
+  xlab("") +
+  ylab(expression(paste("C"[i], " (", mu, "mol . mol"^-1, ")")))+  # fix legend
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
+  scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
+  ylim(0,500) 
+
+
+ggsave(here("figs","Ci.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
+
+# iWUE
 ggplot(dados, aes(treatment, iwue)) +
   geom_errorbar(stat = 'boxplot', width = 0.2) +
   geom_boxplot(aes(fill = treatment), show.legend = F) +
@@ -88,11 +257,13 @@ ggplot(dados, aes(treatment, iwue)) +
   ylab(expression(paste("WUE"[i], " (", mu, "mol mol"^-1, ")")))+  # fix legend
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(size = 15)) + # Axis text size
+  theme(axis.title = element_text(size = 18)) + #Axix title size
   scale_fill_manual(values = c("control" = "cyan3", "drought" = "coral1", "recovery" = "darkolivegreen2"))+
   ylim(0,0.4)                                                       # Fix scale 
 
 
   
-  ggsave(here("figs","iWUE.tiff"), width = 8, height = 6, dpi = 300)
+  ggsave(here("figs","iWUE.tiff"), width = 90, height = 90, units = "mm", dpi = 300)
   
 
